1b9d0ef2b0868122817efd7c79e7530596fd8d98
Alex Place <alex.place.7@gmail.com>
2015-02-06 23:23:05 -0500

    initial commit

---
+++ android/src/com/gdxjam/base/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gdxjam.base.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gdxjam.base.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
---
+++ desktop/src/com/gdxjam/base/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdxjam.base.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdxjam.base.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
