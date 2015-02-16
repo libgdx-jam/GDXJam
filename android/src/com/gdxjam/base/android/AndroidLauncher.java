03dabdb0fd6af5ef5784d795d3953ca4e7175ce5
Twiebs <torinwiebelt@gmail.com>
2015-02-15 22:57:03 -0500

    Revert "Android build now uses immersive mode."
    This reverts commit fee8b18cf00d8d566e35ae2d97d413d1f15c36af.

--- android/src/com/gdxjam/base/android/AndroidLauncher.java
+++ android/src/com/gdxjam/base/android/AndroidLauncher.java
@@ -11,7 +11,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.useImmersiveMode = true;
 		initialize(new Main(), config);
 	}
 }
