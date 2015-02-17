a394d05dca58447ccc9ca6b95bfe2f713e9cfcb4
Alex Place <alex.place.7@gmail.com>
2015-02-16 23:54:40 -0500

    Updated Ashley to 1.5.1

---
+++ core/src/com/gdxjam/test/PathfinderTest.java
@@ -0,0 +1,13 @@
+package com.gdxjam.test;
+
+public class PathfinderTest {
+	
+	public PathfinderTest() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void pathfind(){
+		//com.badlogic.gdx.ai.
+	}
+
+}
