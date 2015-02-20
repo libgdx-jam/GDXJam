2c31864f2747d8edc2fc5312f997e7001f66a2a0
Alex Place <alex.place.7@gmail.com>
2015-02-20 03:48:27 -0500

    Added a selector screen

--- core/src/com/gdxjam/Main.java
+++ core/src/com/gdxjam/Main.java
@@ -3,6 +3,7 @@ package com.gdxjam;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.assets.AssetManager;
 import com.gdxjam.screens.GameScreen;
+import com.gdxjam.screens.SelectorScreen;
 import com.gdxjam.screens.TestScreen2;
 
 public class Main extends Game {
@@ -10,7 +11,7 @@ public class Main extends Game {
 	@Override
 	public void create() {
 		GameManager.init(this);
-		GameManager.setScreen(new TestScreen2());
+		GameManager.setScreen(new SelectorScreen());
 	}
-	
+
 }
