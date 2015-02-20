bc7988a3ed96f19fc49fd30d33ac7484db295934
Alex Place <alex.place.7@gmail.com>
2015-02-20 10:57:51 -0500

    Added back button to abstract screen (ESC)

--- core/src/com/gdxjam/screens/AbstractScreen.java
+++ core/src/com/gdxjam/screens/AbstractScreen.java
@@ -1,11 +1,12 @@
 package com.gdxjam.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.gdxjam.GameManager;
 
 public abstract class AbstractScreen implements Screen {
-	
 
 	@Override
 	public void show() {
@@ -14,6 +15,9 @@ public abstract class AbstractScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
+		if (Gdx.input.isKeyJustPressed(Keys.ESCAPE)) {
+			GameManager.setScreen(new SelectorScreen());
+		}
 		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		Gdx.gl20.glClearColor(0, 0, 0, 1);
 
