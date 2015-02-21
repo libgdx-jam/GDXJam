f9786be887d197154975304bf0ad94fb2742c417
Twiebs <torinwiebelt@gmail.com>
2015-02-21 09:08:10 -0500

    added UI for actions

--- core/src/com/gdxjam/systems/HUDSystem.java
+++ core/src/com/gdxjam/systems/HUDSystem.java
@@ -13,22 +13,36 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
 import com.gdxjam.Assets;
 import com.gdxjam.ai.Squad;
+import com.gdxjam.input.Keybinds;
 import com.gdxjam.utils.Constants;
 
 public class HUDSystem extends EntitySystem implements Disposable{
 	
 	private Stage stage;
-	private Skin skin;
-	
 	private Table hotkeyTable;
 	private Array<TextButton> squadButtons;
+	
+	private Skin skin;
+	private TextButtonStyle hotkeyStyle;
 	
 	public HUDSystem (Skin skin) {
 		this.stage = new Stage();
 		this.skin = skin;
 		this.squadButtons = new Array<TextButton>(true, Constants.maxSquads);
 		
+		initSkin();
 		initGUI();
+	}
+	
+	public void initSkin(){
+		NinePatchDrawable draw = new NinePatchDrawable(
+			Assets.getInstance().hotkey.button);
+		
+		hotkeyStyle = new ImageTextButtonStyle();
+		hotkeyStyle.up = draw;
+		hotkeyStyle.down = draw.tint(Color.GREEN);
+		hotkeyStyle.checked = draw;
+		hotkeyStyle.font = Assets.getInstance().fonts.medium;
 	}
 	
 	public void initGUI(){
@@ -40,6 +54,27 @@ public class HUDSystem extends EntitySystem implements Disposable{
 		
 		bottomTable.add(hotkeyTable);
 		stage.addActor(bottomTable);
+		
+		Table actionTableContainer = new Table();
+		actionTableContainer.setFillParent(true);
+		actionTableContainer.bottom().left();
+		actionTableContainer.add(createActionTable());
+		stage.addActor(actionTableContainer);
+	}
+	
+	public Table createActionTable(){
+		Table actionTable = new Table();
+		TextButton actionButon0 = new TextButton("Q", hotkeyStyle);
+		TextButton actionButon1 = new TextButton("W", hotkeyStyle);
+		TextButton actionButon2 = new TextButton("E", hotkeyStyle);
+		TextButton actionButon3 = new TextButton("R", hotkeyStyle);
+		TextButton actionButon4 = new TextButton("T", hotkeyStyle);
+		actionTable.add(actionButon0);
+		actionTable.add(actionButon1);
+		actionTable.add(actionButon2);
+		actionTable.add(actionButon3);
+		actionTable.add(actionButon4);
+		return actionTable;
 	}
 	
 	public void packHotkeyTable(){
@@ -50,16 +85,7 @@ public class HUDSystem extends EntitySystem implements Disposable{
 	}
 	
 	public void addSquad(Squad squad){
-		NinePatchDrawable draw = new NinePatchDrawable(
-			Assets.getInstance().hotkey.button);
-		
-		TextButtonStyle style = new ImageTextButtonStyle();
-		style.up = draw;
-		style.down = draw.tint(Color.GREEN);
-		style.checked = draw;
-		style.font = Assets.getInstance().fonts.medium;
-
-		TextButton btn = new TextButton(squad.index + 1 + "", style);
+		TextButton btn = new TextButton(squad.index + 1 + "", hotkeyStyle);
 		btn.setColor(Color.DARK_GRAY);
 		squadButtons.insert(squad.index, btn);
 		
