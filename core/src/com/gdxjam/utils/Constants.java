84675eb20089529d5fa62b6dbdb0085db8c69422
Twiebs <torinwiebelt@gmail.com>
2015-02-20 19:47:28 -0500

    Lighting system now blends colors properly

--- core/src/com/gdxjam/utils/Constants.java
+++ core/src/com/gdxjam/utils/Constants.java
@@ -9,15 +9,22 @@ public class Constants {
 	//Time
 	public static final float secondsPerMinute = 0.1f;
 	public static final float secondsPerHour = secondsPerMinute * 60;
-	public static final float dawn = 6 * secondsPerHour;
-	public static final float dusk = 18 * secondsPerHour;
-	public static final float transitionTime = 1.0f * secondsPerHour;
+	
+	public static final float sunriseBegin = 6 * secondsPerHour;
+	public static final float sunriseEnd = 7 * secondsPerHour;
+	public static final float sunriseDuration = sunriseEnd - sunriseBegin;
+	
+	public static final float sunsetBegin = 18 * secondsPerHour;
+	public static final float sunsetEnd = 19 * secondsPerHour;
+	public static final float sunsetDuration = sunsetEnd - sunsetBegin;
 
+	//Keybinds
 	public static final int HOTKEY_PLATOON1 = Keys.NUM_1;
 	public static final int HOTKEY_PLATOON2 = Keys.NUM_2;
 	public static final int HOTKEY_PLATOON3 = Keys.NUM_3;
 	public static final int HOTKEY_PLATOON4 = Keys.NUM_4;
-
+	
+	
 	public static final String BLOCK_TYPE = "tiletype";
 
 	public enum BLOCK_TYPE {
