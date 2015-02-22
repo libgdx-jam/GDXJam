6278e22838dc7269d4ab7bc6fd1b0679f2586c4c
Alex Place <alex.place.7@gmail.com>
2015-02-21 23:38:27 -0500

    Made some systems implement disposable properly

--- core/src/com/gdxjam/systems/EntityRenderSystem.java
+++ core/src/com/gdxjam/systems/EntityRenderSystem.java
@@ -7,45 +7,53 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Disposable;
 import com.gdxjam.components.Components;
 import com.gdxjam.components.SpriteComponent;
 import com.gdxjam.components.SteerableBodyComponent;
 
-public class EntityRenderSystem extends IteratingSystem{
-	
+public class EntityRenderSystem extends IteratingSystem implements Disposable {
+
 	private static final int spriteRotationOffset = -90;
-	
+
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
-	
-	
-	public EntityRenderSystem(OrthographicCamera camera){
+
+	public EntityRenderSystem(OrthographicCamera camera) {
 		super(Family.all(SpriteComponent.class).get());
-		
+
 		this.camera = camera;
 		batch = new SpriteBatch();
 	}
-	
+
 	@Override
-	public void update (float deltaTime) {
+	public void update(float deltaTime) {
 		batch.begin();
 		batch.setProjectionMatrix(camera.combined);
 		super.update(deltaTime);
 		batch.end();
 	}
-	
+
 	@Override
-	protected void processEntity (Entity entity, float deltaTime) {
+	protected void processEntity(Entity entity, float deltaTime) {
 		SpriteComponent spriteComp = Components.SPRITE.get(entity);
-		
-		if(Components.STEERABLE_BODY.has(entity)){
-			SteerableBodyComponent physics = Components.STEERABLE_BODY.get(entity);
+
+		if (Components.STEERABLE_BODY.has(entity)) {
+			SteerableBodyComponent physics = Components.STEERABLE_BODY
+					.get(entity);
 			Vector2 pos = physics.body.getPosition();
 			spriteComp.sprite.setCenter(pos.x, pos.y);
-			spriteComp.sprite.setRotation((MathUtils.radiansToDegrees * physics.body.getAngle()) + spriteRotationOffset);
+			spriteComp.sprite
+					.setRotation((MathUtils.radiansToDegrees * physics.body
+							.getAngle()) + spriteRotationOffset);
 		}
-		
+
 		spriteComp.sprite.draw(batch);
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
 	}
 
 }
