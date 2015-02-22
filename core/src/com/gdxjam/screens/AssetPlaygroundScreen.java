f1e6a0bc09902bc2c699311c45408d99d750124d
Alex Place <alex.place.7@gmail.com>
2015-02-22 01:31:50 -0500

    Added a get method for hotkey table

--- core/src/com/gdxjam/screens/AssetPlaygroundScreen.java
+++ core/src/com/gdxjam/screens/AssetPlaygroundScreen.java
@@ -2,15 +2,16 @@ package com.gdxjam.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.gdxjam.EntityManager;
 import com.gdxjam.GameWorld;
-import com.gdxjam.input.DefaultInputProcessor;
 import com.gdxjam.input.DesktopInputProcessor;
 import com.gdxjam.systems.HUDSystem;
+import com.gdxjam.ui.HotkeyTable;
 
 public class AssetPlaygroundScreen extends AbstractScreen {
 
@@ -19,16 +20,24 @@ public class AssetPlaygroundScreen extends AbstractScreen {
 	Stage stage;
 	Viewport viewport;
 
+	HUDSystem hud;
+	HotkeyTable hotkey;
+
 	@Override
 	public void show() {
 		super.show();
 		EntityManager.getInstance().initSystems();
 		EntityManager.getInstance().loadWorld(new GameWorld(64, 36));
+		hud = EntityManager.getInstance().getSystem(HUDSystem.class);
+		hotkey = hud.getHotkeyTable();
+		hotkey.addHotkey(Keys.Q, "Q");
+		hotkey.addHotkey(Keys.W, "W");
+		hotkey.addHotkey(Keys.E, "E");
+		hotkey.addHotkey(Keys.R, "R");
+		hotkey.addHotkey(Keys.T, "T");
 
 		InputMultiplexer multiplexer = new InputMultiplexer();
-		multiplexer.addProcessor(EntityManager.getInstance()
-				.getSystem(HUDSystem.class).getStage());
-		multiplexer.addProcessor(new DefaultInputProcessor());
+		multiplexer.addProcessor(hud.getStage());
 		multiplexer.addProcessor(new DesktopInputProcessor(EntityManager
 				.getInstance()));
 		Gdx.input.setInputProcessor(multiplexer);
