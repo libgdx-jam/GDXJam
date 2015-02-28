efd1a6a96c16f5a9adcf8e82719e7ba8566ff7fa
Alex Place <alex.place.7@gmail.com>
2015-02-27 20:57:57 -0500

    Removed ninepatch system

--- core/src/com/gdxjam/components/Components.java
+++ core/src/com/gdxjam/components/Components.java
@@ -3,22 +3,32 @@ package com.gdxjam.components;
 import com.badlogic.ashley.core.ComponentMapper;
 
 public class Components {
-	
-	public static final ComponentMapper<SpriteComponent> SPRITE = ComponentMapper.getFor(SpriteComponent.class);
-	public static final ComponentMapper<NinePatchComponent> NINE_PATCH = ComponentMapper.getFor(NinePatchComponent.class);
-	
-	public static final ComponentMapper<HealthComponent> HEALTH = ComponentMapper.getFor(HealthComponent.class);
-	public static final ComponentMapper<PhysicsComponent> PHYSICS = ComponentMapper.getFor(PhysicsComponent.class);
-	public static final ComponentMapper<FactionComponent> FACTION = ComponentMapper.getFor(FactionComponent.class);
-	public static final ComponentMapper<ResourceComponent> RESOURCE = ComponentMapper.getFor(ResourceComponent.class);
-	
-	public static final ComponentMapper<SquadMemberComponent> SQUAD_MEMBER = ComponentMapper.getFor(SquadMemberComponent.class);
-	
-	//AI
-	public static final ComponentMapper<UnitComponent> UNIT = ComponentMapper.getFor(UnitComponent.class);
-	public static final ComponentMapper<SteeringBehaviorComponent> STEERING_BEHAVIOR = ComponentMapper.getFor(SteeringBehaviorComponent.class);
-	public static final ComponentMapper<SteerableComponent> STEERABLE = ComponentMapper.getFor(SteerableComponent.class);
 
-    public static final ComponentMapper<StateMachineComponent> STATE_MACHINE = ComponentMapper.getFor(StateMachineComponent.class);
-    public static final ComponentMapper<ProximityComponent> PROXIMITY = ComponentMapper.getFor(ProximityComponent.class);
+	public static final ComponentMapper<SpriteComponent> SPRITE = ComponentMapper
+			.getFor(SpriteComponent.class);
+
+	public static final ComponentMapper<HealthComponent> HEALTH = ComponentMapper
+			.getFor(HealthComponent.class);
+	public static final ComponentMapper<PhysicsComponent> PHYSICS = ComponentMapper
+			.getFor(PhysicsComponent.class);
+	public static final ComponentMapper<FactionComponent> FACTION = ComponentMapper
+			.getFor(FactionComponent.class);
+	public static final ComponentMapper<ResourceComponent> RESOURCE = ComponentMapper
+			.getFor(ResourceComponent.class);
+
+	public static final ComponentMapper<SquadMemberComponent> SQUAD_MEMBER = ComponentMapper
+			.getFor(SquadMemberComponent.class);
+
+	// AI
+	public static final ComponentMapper<UnitComponent> UNIT = ComponentMapper
+			.getFor(UnitComponent.class);
+	public static final ComponentMapper<SteeringBehaviorComponent> STEERING_BEHAVIOR = ComponentMapper
+			.getFor(SteeringBehaviorComponent.class);
+	public static final ComponentMapper<SteerableComponent> STEERABLE = ComponentMapper
+			.getFor(SteerableComponent.class);
+
+	public static final ComponentMapper<StateMachineComponent> STATE_MACHINE = ComponentMapper
+			.getFor(StateMachineComponent.class);
+	public static final ComponentMapper<ProximityComponent> PROXIMITY = ComponentMapper
+			.getFor(ProximityComponent.class);
 }
