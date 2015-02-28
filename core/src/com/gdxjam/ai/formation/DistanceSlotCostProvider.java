2d4faab77b13bd16293b7c759e3c1a387837dc63
Twiebs <torinwiebelt@gmail.com>
2015-02-28 12:00:15 -0500

    removed setPosition from Loc2

--- core/src/com/gdxjam/ai/formation/DistanceSlotCostProvider.java
+++ core/src/com/gdxjam/ai/formation/DistanceSlotCostProvider.java
@@ -7,12 +7,6 @@ import com.badlogic.gdx.math.Vector2;
 public class DistanceSlotCostProvider implements SlotCostProvider<Vector2>{
 
 	@Override
-	public void calculateCosts () {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public float getCost (FormationMember<Vector2> member, int slotNumber) {
 		boolean isCorrectSlot = slotNumber % 2 == 0;
 		float cost = isCorrectSlot ? 0f : 10000f * 2;
