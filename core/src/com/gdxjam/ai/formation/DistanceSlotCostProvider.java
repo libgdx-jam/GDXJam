5c4d8d4e8a655b43bb71e3cbfe3ffe2b86307686
Alex Place <alex.place.7@gmail.com>
2015-02-28 16:51:31 -0500

    Revert previous commit

--- core/src/com/gdxjam/ai/formation/DistanceSlotCostProvider.java
+++ core/src/com/gdxjam/ai/formation/DistanceSlotCostProvider.java
@@ -4,19 +4,13 @@ import com.badlogic.gdx.ai.fma.FormationMember;
 import com.badlogic.gdx.ai.fma.SoftRoleSlotAssignmentStrategy.SlotCostProvider;
 import com.badlogic.gdx.math.Vector2;
 
-public class DistanceSlotCostProvider implements SlotCostProvider<Vector2>{
+public class DistanceSlotCostProvider implements SlotCostProvider<Vector2> {
 
 	@Override
-	public float getCost (FormationMember<Vector2> member, int slotNumber) {
+	public float getCost(FormationMember<Vector2> member, int slotNumber) {
 		boolean isCorrectSlot = slotNumber % 2 == 0;
 		float cost = isCorrectSlot ? 0f : 10000f * 2;
 		return cost + 1;
-	}
-
-	@Override
-	public void calculateCosts() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
