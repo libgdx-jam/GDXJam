c42ea4e8c3c923b6b8fd5775cb606f3af021e27d
davebaol <davix7@yahoo.it>
2015-03-02 15:34:34 +0100

    Added wedge formation.

--- core/src/com/gdxjam/ai/formation/SquadFormationPattern.java
+++ core/src/com/gdxjam/ai/formation/SquadFormationPattern.java
@@ -13,7 +13,8 @@ public class SquadFormationPattern {
 		Line,
 		Square,
 		Ring,
-		V;
+		V,
+		Wedge;
 	}
 	
 	public static ObjectMap<PatternType, FormationPattern<Vector2>> patterns;
@@ -25,6 +26,7 @@ public class SquadFormationPattern {
 		patterns.put(PatternType.Square, new SquareFormationPattern(Constants.unitRadius * 1.25f));
 		patterns.put(PatternType.Ring, new DefensiveCircleFormationPattern<Vector2>(Constants.unitRadius * 1.25f));
 		patterns.put(PatternType.V, new VFormationPattern(30 * MathUtils.degreesToRadians, Constants.unitRadius * 1.25f));
+		patterns.put(PatternType.Wedge, new WedgeFormationPattern(Constants.unitRadius * 1.25f));
 	}
 	
 }
---
+++ core/src/com/gdxjam/ai/formation/WedgeFormationPattern.java
@@ -0,0 +1,48 @@
+package com.gdxjam.ai.formation;
+
+import com.badlogic.gdx.ai.fma.FormationPattern;
+import com.badlogic.gdx.ai.utils.Location;
+import com.badlogic.gdx.math.Vector2;
+
+public class WedgeFormationPattern implements FormationPattern<Vector2> {
+
+	private int numberOfSlots;
+	private float memberRadius;
+
+	public WedgeFormationPattern(float memberRadius) {
+		this.memberRadius = memberRadius;
+	}
+
+	@Override
+	public void setNumberOfSlots(int numberOfSlots) {
+		this.numberOfSlots = numberOfSlots;
+	}
+
+	@Override
+	public Location<Vector2> calculateSlotLocation(
+			Location<Vector2> outLocation, int slotNumber) {
+		int row = calculateRow(slotNumber);
+		float col = calculateColumn(slotNumber, row);
+		float offset = memberRadius * (numberOfSlots - 1);
+		float memberDiameter = memberRadius + memberRadius;
+		outLocation.getPosition().set(col * memberDiameter - offset, row * memberDiameter);
+		outLocation.setOrientation(0);
+		return outLocation;
+	}
+
+	@Override
+	public boolean supportsSlots(int slotCount) {
+		return true;
+	}
+
+	private int calculateRow(int slotNumber) {
+		double r = (Math.sqrt(1 + 8 * (slotNumber+1)) - 1) * .5;
+		return (int)Math.ceil(r) - 1;
+	}
+	
+	private float calculateColumn(int slotNumber, int row) {
+		int r = row * (row + 1) / 2;
+		return slotNumber - r - row * .5f;
+	}
+
+}
