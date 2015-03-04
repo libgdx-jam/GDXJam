c8b36271cf39fefb496a8c2be426b1c0c576d5f7
davebaol <davix7@yahoo.it>
2015-03-04 02:06:35 +0100

    Slot cost calculation

--- core/src/com/gdxjam/ai/formation/DistanceSlotCostProvider.java
+++ core/src/com/gdxjam/ai/formation/DistanceSlotCostProvider.java
@@ -9,18 +9,14 @@ import com.gdxjam.components.SquadMemberComponent;
 
 public class DistanceSlotCostProvider implements SlotCostProvider<Vector2> {
 	
-	private static final Vector2 tmp = new Vector2();
-
 	@Override
 	public float getCost(FormationMember<Vector2> member, int slotNumber) {
 		SquadMemberComponent squadMemberComp = (SquadMemberComponent) member;
 		SquadComponent squadComp = Components.SQUAD.get(squadMemberComp.squad);
 		
-		tmp.set(squadComp.formation.getSlot(slotNumber).member.getTargetLocation().getPosition());
-		float cost = squadMemberComp.body.getPosition().dst(tmp);
-		return cost;
-		
-//		return 0;
-	
+		Vector2 targetPosition = squadComp.formation.getSlot(slotNumber).member.getTargetLocation().getPosition();
+
+		// The cost is the square distance between current position and target position
+		return squadMemberComp.body.getPosition().dst2(targetPosition);
 	}
 }
