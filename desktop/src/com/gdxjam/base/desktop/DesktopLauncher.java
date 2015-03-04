9ec4fc96560a9a27da78dedbd8cbb04ad8254db6
Alex Place <alex.place.7@gmail.com>
2015-03-03 22:15:08 -0500

    Added icon to desktop launcher

--- desktop/src/com/gdxjam/base/desktop/DesktopLauncher.java
+++ desktop/src/com/gdxjam/base/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.gdxjam.base.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
@@ -8,8 +9,6 @@ import com.gdxjam.Assets;
 import com.gdxjam.Main;
 
 public class DesktopLauncher {
-
-
 
 	public static void main(String[] arg) {
 
@@ -29,6 +28,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
+		config.addIcon("icon.png", FileType.Internal);
 
 		new LwjglApplication(new Main(), config);
 
